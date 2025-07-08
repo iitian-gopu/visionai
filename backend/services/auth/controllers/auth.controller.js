e3272ef99e210528cb62594714628c03bce8250c
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-07-08 15:51:44 +0530

    Create auth auth controller

---
+++ backend/services/auth/controllers/auth.controller.js
@@ -0,0 +1,39 @@
+import { getAuth } from "firebase-admin/auth"
+import { app } from "../config/firebase.js"
+import User from "../models/user.model.js"
+import { createConnection } from "mongoose"
+import redis from "../../../shared/redis/redis.js"
+
+export const login = async (req, res) => {
+    try {
+        const { token } = req.body
+        const decoded = await getAuth(app).verifyIdToken(token)
+        let user = await User.findOne({
+            firebaseUid: decoded.uid
+        })
+
+        if (!user) {
+            user = await User.create({
+                firebaseUid: decoded.uid,
+                name: decoded.name,
+                email: decoded.email,
+                avatar: decoded.picture
+            })
+        }
+
+        const sessionId = crypto.randomUUID()
+        await redis.set(`user-session-${user?._id}`,
+            sessionId
+            , "EX", 7 * 24 * 60 * 60)
+        await redis.set(`session-${sessionId}`, JSON.stringify({
+            userId: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            plan: user.plan,
+            credits: user.credits,
+            totalCredits: user.totalCredits,
+            planExpiresAt: user.planExpiresAt
+        }), "EX", 7 * 24 * 60 * 60)
+
+
