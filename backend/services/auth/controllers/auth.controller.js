2ff5d59a67548197ac4e4c8df7efa65f2ed32e61
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-07-10 20:57:02 +0530

    Improve auth auth controller

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -37,3 +37,41 @@ export const login = async (req, res) => {
         }), "EX", 7 * 24 * 60 * 60)
 
 
+
+
+        res.cookie("session", sessionId, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json(user)
+
+    } catch (error) {
+        return res.status(500).json({ message: `login error ${error}` })
+    }
+}
+
+
+export const logOut = async (req, res) => {
+    try {
+        const sessionId = req.cookies?.session
+        await redis.del(`session-${sessionId}`)
+
+        res.clearCookie("session")
+        return res.status(200).json({ message: "logout successfully" })
+    } catch (error) {
+        return res.status(500).json({ message: `logout error ${error}` })
+    }
+}
+
+
+export const updateUserPayment = async (req, res) => {
+    try {
+        const { plan, credits, userId } = req.body
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        user.plan = plan
