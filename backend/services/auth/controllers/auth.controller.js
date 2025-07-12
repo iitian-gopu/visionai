4df3cd326e106f4137876f599a92b1ab7c9539f6
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-07-12 20:45:18 +0530

    Improve auth auth controller

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -75,3 +75,42 @@ export const updateUserPayment = async (req, res) => {
             return res.status(404).json({ message: "User not found" })
         }
         user.plan = plan
+        user.credits += credits
+        user.totalCredits += credits
+        user.planExpiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+        await user.save()
+
+        const sessionId = await redis.get(`user-session-${user?._id}`)
+        console.log("sessionId", sessionId)
+        await redis.set(`session-${sessionId}`, JSON.stringify({
+            userId: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            plan: user.plan,
+            credits: user.credits,
+            totalCredits: user.totalCredits,
+            planExpiresAt: user.planExpiresAt
+        }), "EX", 7 * 24 * 60 * 60)
+
+        return res.status(200).json({ success: true })
+
+    } catch (error) {
+        return res.status(500).json({ message: `update user payment error ${error}` })
+    }
+}
+
+
+export const deductCredits = async (req, res) => {
+    try {
+        const { userId, agent } = req.body
+        
+        const COST = {
+
+            chat: 1,
+
+            search: 5,
+
+            coding: 10,
+
+            pdf: 10,
