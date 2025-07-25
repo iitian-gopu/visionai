be680a2d884c68b5bbe50b78f3ce0bc83b3416d4
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-07-25 09:56:22 +0530

    Create chat chat controller

---
+++ backend/services/chat/controllers/chat.controller.js
@@ -0,0 +1,25 @@
+import Conversation from "../models/coversation.model.js"
+import Message from "../models/message.model.js"
+
+export const createConversation=async (req,res) => {
+  try {
+    const userId=req.headers["x-user-id"]
+    console.log("userId",userId)
+    const conversation=await Conversation.create({
+        userId:userId
+    })
+
+    return res.status(200).json(conversation)
+  } catch (error) {
+     return res.status(500).json({message:`create conversation error ${error}`})
+  }
+}
+
+export const getConversations=async (req,res) => {
+  try {
+    const userId=req.headers["x-user-id"]
+    console.log("userId",userId)
+    const conversations=await Conversation.find({
+        userId:userId
+    }).sort({updatedAt:-1})
+
