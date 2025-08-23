eae91d46823e5481f0ce210fdfe4d2fa0a706b48
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-08-23 10:51:53 +0530

    Continue work on agent graph

--- backend/services/agent/graph/graph.js
+++ backend/services/agent/graph/graph.js
@@ -21,3 +21,26 @@ workflow.addNode("ppt",pptAgent)
 workflow.addNode("vision",visionAgent)
 workflow.addNode("pdfRag",pdfRag)
 workflow.addNode("imageAnalyzer",imageAnalyzer)
+
+workflow.addEdge("__start__","router")
+workflow.addConditionalEdges("router",(state)=>{
+   switch (state.agent) {
+    case "chat":
+     return "chat";
+    case "search":
+     return "search";
+    case "coding":
+     return "coding";
+    case "pdf":
+     return "pdf";
+    case "ppt":
+     return "ppt";
+    case "vision":
+     return "vision";
+    case "pdfRag":
+     return "pdfRag";
+     case "imageAnalyzer":
+     return "imageAnalyzer";  
+    default:
+     return "chat"
+   }
