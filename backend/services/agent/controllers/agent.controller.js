c8d4fdaf09bb97224b090c9991c7ef880fa1e88e
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-08-24 09:05:42 +0530

    Polish agent agent controller

--- backend/services/agent/controllers/agent.controller.js
+++ backend/services/agent/controllers/agent.controller.js
@@ -15,3 +15,20 @@ export const agent=async (req,res,next) => {
         })
         const result=await graph.invoke({
             prompt,conversationId,agent,userId,file
+        })
+        console.log("result",result)
+       await addMessage(conversationId,"user",prompt)
+        await addMessage(conversationId,"assistant",result.aiResponse)
+        await axios.post(`${process.env.CHAT_SERVICE}/save-message`,{
+            conversationId,role:"assistant",content:result?.aiResponse,images:result?.images,artifacts:result?.artifacts
+        })
+        return res.status(200).json({
+            answer:result?.aiResponse,
+            images:result?.images,
+            artifacts:result?.artifacts
+        })
+       
+    } catch (error) {
+       next(error)
+    }
+}
