a3bf5c746bb15b8810212ad02f5ae2f0ed454ce4
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-09-09 19:50:47 +0530

    Polish agent multer

--- backend/services/agent/config/multer.js
+++ backend/services/agent/config/multer.js
@@ -17,3 +17,21 @@ const storage = multer.diskStorage({
     },
 })
 
+const fileFilter = (req, file, cb) => {
+     if(file.mimetype=="application/pdf" ||
+        file.mimetype.startsWith("image/")
+      ){
+          
+        cb(null,true)
+
+      }else{
+        cb(new Error("Only PDF and Images are allowed."))
+      }
+}
+
+
+export default  multer({
+    storage, fileFilter, limits: {
+        fileSize: 20 * 1024 * 1024
+    }
+})
