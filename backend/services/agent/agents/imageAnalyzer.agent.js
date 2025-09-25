6584457841185a79c2996003d2bd7d0ec907dfb2
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-09-25 12:21:53 +0530

    Add agent imageAnalyzer agent

---
+++ backend/services/agent/agents/imageAnalyzer.agent.js
@@ -0,0 +1,23 @@
+
+import { HumanMessage, SystemMessage } from "@langchain/core/messages"
+import { getModel } from "../config/llmModels.js"
+import fs from "fs/promises"
+import { deductCredits } from "../utils/deductCredits.js"
+import { checkAgentLimit } from "../config/agentLimit.js"
+export const imageAnalyzer =async (state) => {
+     await checkAgentLimit(state.userId,"image")
+    try {
+        const llm = await getModel("imageAnalyzer")
+
+        const imageBuffer = await fs.readFile(state.file.path)
+        const base64Image = imageBuffer.toString("base64")
+
+        const messages = [
+            new SystemMessage(
+                `You are CortexAI image analyzer Agent.
+
+Rules:
+
+- Analyze only the uploaded image.
+- Answer the user's question accurately.
+- If text exists in the image, extract it.
