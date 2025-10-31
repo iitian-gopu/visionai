2262f917582aacb4b556aa68df0268e4aa2e1efa
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-10-31 18:32:11 +0530

    Finish conversationSlice redux slice

--- frontend/src/redux/conversationSlice.js
+++ frontend/src/redux/conversationSlice.js
@@ -18,3 +18,22 @@ const conversationSlice=createSlice({
        },
 
       setConvTitle:(state,action)=>{
+           const {title,conversationId}=action.payload
+           state.conversations=state.conversations.map((conv)=>(
+            conv._id==conversationId?(
+             { ...conv,title}
+            ):conv
+           )) 
+
+           if(state.selectedConversation?._id==conversationId){
+               state.selectedConversation={...state.selectedConversation,title}
+           }
+      }
+
+    }
+   
+})
+
+export const {setConversations,addConversation,setSelectedConversation,setConvTitle}=conversationSlice.actions 
+export default conversationSlice.reducer
+
