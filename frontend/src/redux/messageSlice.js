1966ed245e8f8c3eaa8b4f1b36a460bd55e4487c
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-11-01 14:07:26 +0530

    Finalize messageSlice redux slice

--- frontend/src/redux/messageSlice.js
+++ frontend/src/redux/messageSlice.js
@@ -14,3 +14,19 @@ const messageSlice=createSlice({
        },
         addMessage:(state,action)=>{
         state.messages.push(action.payload)
+       },
+       setArtifacts:(state,action)=>{
+        state.artifacts=action.payload
+       },
+       setIsLoading:(state,action)=>{
+        state.isLoading=action.payload
+       }
+      
+
+    }
+   
+})
+
+export const {setMessages,addMessage,setArtifacts,setIsLoading}=messageSlice.actions 
+export default messageSlice.reducer
+
