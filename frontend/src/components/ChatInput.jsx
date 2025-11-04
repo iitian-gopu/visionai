db4a5e62dbd4cf454bf32ae3dd6404cb88936a03
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2025-11-04 15:55:40 +0530

    Set up ChatInput component

---
+++ frontend/src/components/ChatInput.jsx
@@ -0,0 +1,54 @@
+import { Code2, FileText, Globe, ImageIcon, MessageSquare, Mic, MicOff, Paperclip, Presentation, Send, X, Zap } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import sendMessage from '../features/sendMessage'
+import { useDispatch, useSelector } from 'react-redux'
+import { addMessage, setArtifacts, setIsLoading, setMessages } from '../redux/messageSlice'
+import { createConversation } from '../features/createConversation'
+import { addConversation, setConvTitle, setSelectedConversation } from '../redux/conversationSlice'
+import { updateConversation } from '../features/updateConversation'
+import { useRef } from 'react'
+
+
+function ChatInput() {
+  const [value, setValue] = useState("")
+  const [selectedAgent, setSelectedAgent] = useState("Auto")
+  const { selectedConversation } = useSelector(state => state.conversation)
+  const { messages, isLoading } = useSelector(state => state.message)
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [listening, setListening] = useState(false)
+  const recognitionRef = useRef(null)
+  const fileRef = useRef(null)
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+    if (!SpeechRecognition) return;
+
+    const recognition = new SpeechRecognition()
+    recognition.lang = "en-US"
+    recognition.interimResults = true;
+    recognition.continuous = true;
+
+    recognition.onresult = (event) => {
+      let transcript = ""
+
+      for (let index = event.resultIndex; index < event.results.length; index++) {
+
+        transcript += event.results[index][0].transcript
+      }
+      setValue(transcript)
+    }
+
+    recognition.onend = () => {
+      setListening(false)
+    }
+
+    recognitionRef.current = recognition
+  }, [])
+
+  const toggleMic = () => {
+    if (!recognitionRef.current) {
+      alert("speech recognition not supported")
+    }
+    if (listening) {
